0f89322b79cfee71cdd1226a88055a51839d493b
Naser Moin @ Testbench Pi <moinnas@colostate.edu>
2023-03-23 13:50:25 -0600

    added the APD_Deploy section to UI setup script

--- UI_setup_script.sh
+++ UI_setup_script.sh
@@ -98,9 +98,34 @@ sudo python setup.py install --qmake=/opt/Qt/6.2.3-aarch64/bin/qmake --cmake=/op
 
 # start UI
 cd ~/projects
-git clone https://github.com/QuercusFelis/EKartUI
+git clone https://github.com/Electric-Go-Kart/EKartUI
 cd EKartUI
-python Main.py
+#at this point you can use the following command to run the UI without APD features
+#python Main.py
 
+#APD setup
+cd ~/projects
+mkdir APD_deploy && cd APD_deploy
+git clone https://github.com/ultralytics/yolov5
+cd yolov5
+git checkout 79bca2bf64da04e7e1e74a132eb54171f41638cc
+cd ..
+git clone https://github.com/Electric-Go-Kart/APD_deploy
+cd APD_deploy
+mv detect.py ../yolov5/
+mv 528Project/ ../yolov5/
+mv run_it.sh ../yolov5/
+cd ..
+rm -rf APD_deploy/
+cd yolov5
+mkdir runs
+# Setup Coral dependencies
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt update
+sudo apt install libedgetpu1-std -y
+sudo apt install python3-pycoral -y
+pip install -r requirements.txt
+pip install numpy --upgrade
 
 
